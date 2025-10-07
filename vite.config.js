eb2ad84565d8343821b05235b81d8d37fc8c248f
LONGEVID <longevidstreaming@gmail.com>
2025-10-07 11:10:10 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,75 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import autoprefixer from 'autoprefixer';
+import path from 'path';
+import fs from 'fs';
+import handlebars from 'handlebars';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
-export default defineConfig({
-  server: {
-    port: 5173
-  }
-})
+const pages = {"index":{"outputDir":"./","lang":"en","title":"","cacheVersion":6,"meta":[{"name":"twitter:card","content":"summary"},{"property":"og:type","content":"website"},{"name":"robots","content":"index, follow"}],"scripts":{"head":"\n","body":"\n"},"baseTag":{"href":"/","target":"_self"},"alternateLinks":[{"rel":"alternate","hreflang":"x-default","href":"https://76d30172-2deb-41d8-b591-720ff83dbd7d.weweb-preview.io/"},{"rel":"alternate","hreflang":"en","href":"https://76d30172-2deb-41d8-b591-720ff83dbd7d.weweb-preview.io/"}]}};
+
+// Read the main HTML template
+const template = fs.readFileSync(path.resolve(__dirname, 'template.html'), 'utf-8');
+const compiledTemplate = handlebars.compile(template);
+
+// Generate an HTML file for each page with its metadata
+Object.values(pages).forEach(pageConfig => {
+    // Compile the template with page metadata
+    const html = compiledTemplate({
+        title: pageConfig.title,
+        lang: pageConfig.lang,
+        meta: pageConfig.meta,
+        scripts: {
+            head: pageConfig.scripts.head,
+            body: pageConfig.scripts.body,
+        },
+        alternateLinks: pageConfig.alternateLinks,
+        cacheVersion: pageConfig.cacheVersion,
+        baseTag: pageConfig.baseTag,
+    });
+
+    // Save output html for each page
+    if (!fs.existsSync(pageConfig.outputDir)) {
+        fs.mkdirSync(pageConfig.outputDir, { recursive: true });
+    }
+    fs.writeFileSync(`${pageConfig.outputDir}/index.html`, html);
+});
+
+const rollupOptionsInput = {};
+for (const pageName in pages) {
+    rollupOptionsInput[pageName] = path.resolve(__dirname, pages[pageName].outputDir, 'index.html');
+}
+
+export default defineConfig(() => {
+    return {
+        plugins: [nodePolyfills({ include: ['events', 'stream', 'string_decoder'] }), vue()],
+        base: "/",
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: 'modern-compiler',
+                },
+            },
+            postcss: {
+                plugins: [autoprefixer],
+            },
+        },
+        build: {
+            chunkSizeWarningLimit: 10000,
+            rollupOptions: {
+                input: rollupOptionsInput,
+                onwarn: (entry, next) => {
+                    if (entry.loc?.file && /js$/.test(entry.loc.file) && /Use of eval in/.test(entry.message)) return;
+                    return next(entry);
+                },
+                maxParallelFileOps: 900,
+            },
+        },
+        logLevel: 'warn',
+    };
+});
